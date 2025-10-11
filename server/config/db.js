8703366711e3ef876251518e7f9a36a86860e142
Devansh Deshpande <devansh436@gmail.com>
2025-10-11 21:19:49 +0530

    feat: deploy

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,19 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
-});
+const pool = process.env.DATABASE_URL
+  ? mysql.createPool(process.env.DATABASE_URL)
+  : mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 3306,
+      waitForConnections: true,
+      connectionLimit: 10,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    });
 
 module.exports = pool;
